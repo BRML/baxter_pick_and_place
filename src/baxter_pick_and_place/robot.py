9f7c5accaa604ead99cd2e29f3d6eec96df67982
lude-ma <ludersdorfer@fortiss.org>
2015-12-09 11:51:18 +0100

    inverse kinematics

--- src/baxter_pick_and_place/robot.py
+++ src/baxter_pick_and_place/robot.py
@@ -26,6 +26,18 @@
 import numpy as np
 import time
 
+import rospy
+
+from tf import transformations
+from geometry_msgs.msg import (
+    Pose,
+    PoseStamped
+)
+from baxter_core_msgs.srv import (
+    SolvePositionIK,
+    SolvePositionIKRequest
+)
+
 import baxter_interface
 from baxter_interface import CHECK_VERSION
 
@@ -33,6 +45,15 @@ from baxter_interface import CHECK_VERSION
 class Robot(object):
 
     def __init__(self):
+        """
+         A baxter research robot instance with some additional functionality.
+        """
+        self._limbs = {
+            'left': baxter_interface.Limb('left'),
+            'right': baxter_interface.Limb('right')
+        }
+        self._top_pose = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
         print("\nGetting robot state ... ")
         self._rs = baxter_interface.RobotEnable(CHECK_VERSION)
         self._init_state = self._rs.state().enabled
@@ -47,6 +68,11 @@ class Robot(object):
         return True
 
     def pick_and_place_object(self):
+        """ Detect, pick up and place an object upon receiving an execution
+        command.
+
+        :return: boolean flag on completion
+        """
         print ' rabbiting away ...'
         # Wait for object to be triggered---dummy
 
@@ -57,6 +83,7 @@ class Robot(object):
 
         _trigger_dummy(10.0)
         # move limb to top-down-view pose
+        self._move_to_pose('left', self._top_pose)
         # record top-down-view image
         # detect object candidates
         # for candidate in candidates:
@@ -77,8 +104,22 @@ class Robot(object):
         print '  something went wrong'
         return False
 
-    def _move_to_pose(self, pose):
-        return True
+    def _move_to_pose(self, limb, pose):
+        """ Move robot limb to Cartesian pose.
+        :type limb: str
+        :param limb: the limb of the robot, <'left', 'right'>
+        :type pose: [float, float, float, float, float, float]
+        :param pose: desired Cartesian pose
+        :return: boolean flag on completion
+        """
+        try:
+            cmd = self._inverse_kinematics(limb, pose)
+        except Exception as e:
+            print e
+            ret = False
+        else:
+            ret = self._limbs[limb].move_to_joint_positions(cmd)
+        return ret
 
     def _perturbe_pose(self):
         return True
@@ -92,3 +133,71 @@ class Robot(object):
 
     def _release_object(self):
         return True
+
+    def _inverse_kinematics(self, limb, pose=None):
+        """ Inverse kinematics of one Baxter arm.
+        Compute valid set of joint angles for a given Cartesian pose using the
+        inverse kinematics service. Return current joint angles if None pose is
+        given.
+        :type limb: str
+        :param limb: the limb of the robot, <'left', 'right'>
+        :type pose: [float, float, float, float, float, float]
+        :param pose: desired Cartesian pose
+        :return: valid set of joint angles
+        """
+        if pose is None:
+            return self._limbs[limb].joint_angles()
+
+        qp = list_to_pose_stamped(pose, "base")
+
+        node = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
+        ik_service = rospy.ServiceProxy(node, SolvePositionIK)
+        ik_request = SolvePositionIKRequest()
+        ik_request.pose_stamp.append(qp)
+        try:
+            rospy.wait_for_service(node, 5.0)
+            ik_response = ik_service(ik_request)
+        except (rospy.ServiceException, rospy.ROSException), error_message:
+            rospy.logerr("Service request failed: %r" % (error_message,))
+            raise
+
+        if ik_response.isValid[0]:
+            # convert response to joint position control dictionary
+            return dict(zip(ik_response.joints[0].name,
+                            ik_response.joints[0].position))
+        else:
+            rospy.logerr("ERROR - inverse kinematics - No valid joint configuration found")
+            raise Exception("ERROR - inverse kinematics - No valid joint configuration found")
+
+
+def list_to_pose(pose_list):
+    pose_msg = Pose()
+    if len(pose_list) == 7:
+        pose_msg.position.x = pose_list[0]
+        pose_msg.position.y = pose_list[1]
+        pose_msg.position.z = pose_list[2]
+        pose_msg.orientation.x = pose_list[3]
+        pose_msg.orientation.y = pose_list[4]
+        pose_msg.orientation.z = pose_list[5]
+        pose_msg.orientation.w = pose_list[6]
+    elif len(pose_list) == 6:
+        pose_msg.position.x = pose_list[0]
+        pose_msg.position.y = pose_list[1]
+        pose_msg.position.z = pose_list[2]
+        q = transformations.quaternion_from_euler(pose_list[3], pose_list[4],
+                                                  pose_list[5])
+        pose_msg.orientation.x = q[0]
+        pose_msg.orientation.y = q[1]
+        pose_msg.orientation.z = q[2]
+        pose_msg.orientation.w = q[3]
+    else:
+        print "Expected either 6 or 7 elements in list: (x,y,z,r,p,y) or (x,y,z,qx,qy,qz,qw)"
+    return pose_msg
+
+
+def list_to_pose_stamped(pose_list, target_frame):
+    pose_msg = PoseStamped()
+    pose_msg.pose = list_to_pose(pose_list)
+    pose_msg.header.frame_id = target_frame
+    pose_msg.header.stamp = rospy.Time.now()
+    return pose_msg
