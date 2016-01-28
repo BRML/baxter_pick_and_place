5b234f9e169afe3eef85f94360826b6204f51d16
lude-ma <ludersdorfer@fortiss.org>
2016-01-28 15:52:24 +0100

    writing object names (label strings) to object_names.txt

---
+++ scripts/assemble_data.py
@@ -0,0 +1,29 @@
+import os
+
+
+objects = ['bin', 'duplo_brick', 'extra_mints', 'glue_stick', 'golf_ball', 'pen']
+
+
+def write_objects(filename):
+    with open(filename, 'w') as thefile:
+        for obj in objects:
+            thefile.write("%s\n" % obj)
+
+
+def read_objects(filename):
+    with open(filename, 'r') as thefile:
+        objects = thefile.read().splitlines()
+    return objects
+
+
+if __name__ == '__main__':
+    script_dirname = os.path.abspath(os.path.dirname(__file__))
+    data_dirname = os.path.abspath(os.path.join(script_dirname, '..', 'data',
+                                                'synthetic_demo_data'))
+    if not os.path.exists(data_dirname):
+        os.makedirs(data_dirname)
+
+    # write object names (label strings) to file,
+    # their order in the file gives the label int's
+    fn = os.path.join(data_dirname, 'object_names.txt')
+    write_objects(filename=fn)
