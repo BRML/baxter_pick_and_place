67ff953648450f36d3e276b4752a8dbc2c734b9a
lude-ma <ludersdorfer@fortiss.org>
2016-01-29 12:14:51 +0100

    function to assemble data set

--- scripts/assemble_data.py
+++ scripts/assemble_data.py
@@ -1,6 +1,10 @@
+import csv
+import glob
+import gzip
 import numpy as np
 import os
 import pandas as pd
+import time
 
 
 objects = ['bin', 'duplo_brick', 'extra_mints', 'glue_stick', 'golf_ball', 'pen']
@@ -25,8 +29,49 @@ def read_objects(filename):
     return objects
 
 
-def assemble_data():
-    pass
+def assemble_data(objects, images, base_dirname, image_dirname, csv_filename,
+                  overwrite=True):
+    """ Generate data set and data set description with a given number of
+    images from a set of base images (background plus foreground objects).
+    :param objects: list of objects to use
+    :param images: number of images to generate
+    :param base_dirname: directory to read base images from
+    :param image_dirname: directory to write generated images to
+    :param csv_filename: CSV file to write generated images' parameter to
+    :param overwrite: Whether to overwrite existing CSV file
+    """
+    csv_header = ['image_name', 'image_filename', 'patch_parameters', 'label']
+
+    # prepare lists of available base images for image generation
+    files = glob.glob(os.path.join(base_dirname, '*.jpg'))
+    background_list = [f for f in files if 'background' in f]
+    object_list = list()
+    for f in files:
+        for obj in objects:
+            if obj in f:
+                object_list.append(f)
+
+    # write header to CSV file
+    if overwrite:
+        with gzip.open(csv_filename, 'w') as fp:
+            csv_writer = csv.writer(fp)
+            csv_writer.writerow(csv_header)
+
+    start = time.time()
+    # generate required number of images with custom properties
+    # and write them to CSV file
+    for idx in range(images):
+        d = _assemble_image(background_list, object_list, image_dirname)
+        with gzip.open(csv_filename, 'a') as fp:
+            csv_writer = csv.writer(fp)
+            csv_writer.writerow(d)
+
+    print 'Wrote %i images in %.3fs.' % (images, time.time() - start)
+
+
+def _assemble_image(bg_list, fg_list, image_dirname):
+    data = ['bla1', 'bla2', ((1, 2, 3), (5, 6, 7), (8, 9, 10), (12, 13, 14)), (0, 1, 2, 3)]
+    return data
 
 
 def create_split(csv_filename, data_dirname, labels, images, split=(.7, .3)):
@@ -76,6 +121,8 @@ def main():
     'augment' the data set (cf. data augmentation).
     """
     script_dirname = os.path.abspath(os.path.dirname(__file__))
+    base_dirname = os.path.abspath(os.path.join(script_dirname, '..', 'data',
+                                                'sdd'))
     data_dirname = os.path.abspath(os.path.join(script_dirname, '..', 'data',
                                                 'synthetic_demo_data'))
     if not os.path.exists(data_dirname):
@@ -83,11 +130,15 @@ def main():
     image_dirname = os.path.join(data_dirname, 'images')
     if not os.path.exists(image_dirname):
         os.makedirs(image_dirname)
+    csv_filename = os.path.join(data_dirname, 'synthetic_demo_data.csv.gz')
 
     # write object names (label strings) to file,
     # their order in the file gives the label int's
     # fn = os.path.join(data_dirname, 'object_names.txt')
     # write_objects(filename=fn)
+
+    assemble_data(objects, 10, base_dirname, image_dirname, csv_filename, overwrite=True)
+    # create_split(csv_filename, data_dirname, 0, 10)
 
 if __name__ == '__main__':
     main()
