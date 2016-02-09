545b69597767bf4cf814fd88b562f91d16f6e9c1
lude-ma <ludersdorfer@fortiss.org>
2016-02-09 11:28:21 +0100

    opencv cascade classifier data set generation, training and testing pipeline implemented

--- scripts/opencv_dat2vec.sh
+++
@@ -1,7 +0,0 @@
-opencv_createsamples \
--vec /home/baxter/ros_ws/src/baxter_pick_and_place/data/sdd/bin_20_20.vec \
--info /home/baxter/ros_ws/src/baxter_pick_and_place/data/sdd/bin_info.dat \
--w 20 \
--h 20 \
--num 210 \
-# -show
