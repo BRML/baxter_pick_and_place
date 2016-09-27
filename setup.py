e75f90352cee1fd273980f70ead72fe3efb685c3
lude-ma <ludersdorfer@fortiss.org>
2016-09-27 10:26:17 +0200

    updated setup.py according to answer

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
+
+# DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+# http://answers.ros.org/question/192723/cant-find-python-scripts-after-sourcing/?answer=192738#post-id-192738
+
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup()
-d['packages'] = ['baxter_pick_and_place']
-d['package_dir'] = {'': 'src'}
 
+d = generate_distutils_setup()
+d['packages'] = ['demo', 'hardware', 'instruction', 'motion_planning', 'servoing', 'simulation', 'vision']
+d['package_dir'] = {'': 'src'}
+d['requires'] = ['rospy', 'roswtf', 'tf',
+                 'geometry_msgs', 'std_msgs', 'std_srvs', 'gazebo_msgs',
+                 'baxter_interface', 'baxter_core_msgs',
+                 'os', 'time', 'logging', 'numpy',
+                 'cv2', 'caffe']
 setup(**d)
