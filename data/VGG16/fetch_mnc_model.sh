3d655ea947118082cbc829d76bed88fe820c2fcd
lude-ma <ludersdorfer@fortiss.org>
2016-12-02 14:57:43 +0100

    download script for pre-trained MNC demo model

--- data/VGG16/fetch_mnc_model.sh
+++ data/VGG16/fetch_mnc_model.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 cd $DIR
 
 FILE=mnc_model.caffemodel.h5
-URL=https://www.brml.tum.de/BaxterCollision/caffe_models/$FILE
+URL="https://onedrive.live.com/download?resid=F371D9563727B96F!91968&authkey=!AGcNz7xSH5E98zg"
 CHECKSUM=c012e6e70ce854b2c3559f6dba7515da
 
 if [ -f $FILE ]; then
@@ -25,6 +25,6 @@ fi
 
 echo "Downloading MNC model..."
 
-wget --user AnomalyWeb@brml.tum.de --ask-password --no-check-certificate $URL -O $FILE
+wget $URL -O $FILE
 
 echo "Done. Please run this command again to verify that checksum = $CHECKSUM."
