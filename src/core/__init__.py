2610c8732b7d071618f33e2055da845d5222cefc
lude-ma <ludersdorfer@fortiss.org>
2016-12-09 12:31:25 +0100

    removed everything from gh-pages branch

--- src/core/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Module for implementing base classes providing core functionality."""
-
-from gitlogger import git_logger
-
-from log_handling import get_default_handler
